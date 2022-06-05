a068578d9e1188c97681906990a0900d30ddcb98
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-06-05 16:13:17 -0400

    test: user_story_2

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
-class Chef <ApplicationRecord
+class Chef < ApplicationRecord
   validates_presence_of :name
-  has_many :dishes
-
+  has_many :dishes, dependent: :destroy
+  has_many :dish_ingredients, through: :dishes, dependent: :destroy
+  has_many :ingredients, through: :dish_ingredients, dependent: :destroy
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Chef, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
+
   describe "relationships" do
-    it {should have_many :dishes}
+    it { should have_many :dishes}
+    it { should have_many(:dish_ingredients).through(:dishes) }
+    it { should have_many(:ingredients).through(:dish_ingredients) }
   end
 end
