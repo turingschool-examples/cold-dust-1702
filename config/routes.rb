d57a4ee71ebd4dbfa07d739a19ddf21fceced038
Sage Skaff <skaffsage@gmail.com>
2022-06-06 13:59:59 -0700

    Add chef ingredients route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :dishes, only: %i[show]
 
-  resources :chefs, only: %i[show]
+  resources :chefs, only: %i[show] do
+    resources :ingredients, controller: :chef_ingredients, only: %i[index]
+  end
 end
