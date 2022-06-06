9dcbb6daf18cf892a763d58b91f1c671aae6949e
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-06 16:12:01 -0600

    Feat: display dish name, description, and ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,13 @@
 @dish_1 = @chef.dishes.create!(name: "Chicken Enchiladas", description: "Spicy enchiladas covered in cheese")
 @dish_2 = @chef.dishes.create!(name: "Roasted Chicken Linguini", description: "Creamy pasta")
 @dish_3 = @chef.dishes.create!(name: "Baked Ziti", description: "Red sauce with italian sausage")
+
+@chicken = Ingredient.create!(name: "Chicken", calories: 400)
+@noodles = Ingredient.create!(name: "Noodles", calories: 200)
+@sausage = Ingredient.create!(name: "Sausage", calories: 500)
+@peppers = Ingredient.create!(name: "Bell Pepper", calories: 50)
+
+DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @chicken.id)
+DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @peppers.id)
+DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @noodles.id)
+DishIngredient.create!(dish_id: @dish_3.id, ingredient_id: @sausage.id)
