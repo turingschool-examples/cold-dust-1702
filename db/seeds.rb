739b41d77b73ae145bdab298a7a3c0ecc7fbf7e7
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-06-06 09:49:39 -0400

    Add seeds, check that code works on server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+chef = Chef.create!(name: "Rob")
+
+dish1 = chef.dishes.create!(name: "Pancakes", description: "Stack of 3 fluffy pancakes")
+dish2 = chef.dishes.create!(name: "Omelette", description: "Served with home fries")
+
+milk = Ingredient.create!(name: "milk", calories: 90)
+flour = Ingredient.create!(name: "flour", calories: 60)
+eggs = Ingredient.create!(name: "eggs", calories: 80)
+potatoes = Ingredient.create!(name: "potatoes", calories: 100)
+
+DishIngredient.create!(dish: dish1, ingredient: milk)
+DishIngredient.create!(dish: dish1, ingredient: flour)
+DishIngredient.create!(dish: dish1, ingredient: eggs)
+DishIngredient.create!(dish: dish2, ingredient: potatoes)
