3e31fbb65804c62abe34ecf2a2bcc7df4b7341e4
Stephen Wilkens <stephenwilkens@gmail.com>
2022-06-06 17:46:09 -0600

    Add ingredents uniq model method and test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,9 @@
 class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :ingredients, through: :dishes
 
+  def ingredients_uniq
+    ingredients.pluck(:name).uniq    
+  end
 end
