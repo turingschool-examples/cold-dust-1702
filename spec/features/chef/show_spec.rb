2ea279238a0f80bd2c8f10d295e0d2ab3f40b7d5
Sage Skaff <skaffsage@gmail.com>
2022-06-06 13:43:57 -0700

    Add chef show page test

---
+++ spec/features/chef/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Chefs Show Page' do
+  let!(:chef) { Chef.create!(name: 'Remy') }
+
+  let!(:dish) { chef.dishes.create!(name: 'Salad', description: 'Green and leafy!') }
+
+  let!(:ingredient1) { Ingredient.create!(name: 'Kale', calories: 100, dish: dish) }
+  let!(:ingredient2) { Ingredient.create!(name: 'Tomato', calories: 50, dish: dish) }
+
+  let!(:dish_ingredient1) { DishIngredient.create!(dish: dish, ingredient: ingredient1) }
+  let!(:dish_ingredient2) { DishIngredient.create!(dish: dish, ingredient: ingredient2) }
+  it 'has chefs name' do
+    visit chef_path(chef.id)
+
+    expect(page).to have_content('Remy')
+  end
+end
