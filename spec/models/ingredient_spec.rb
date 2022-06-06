647fbe18b0505f993c382c7e7c3c3bf14e2f4908
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-06 15:45:32 -0600

    Test: ingredient references

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper' 
+
+RSpec.describe Ingredient, type: :model do
+
+  describe 'references' do
+    it { should have_many :dish_ingredients }
+    it { should have_many :dishes, through: :dish_ingredients }
+  end
+  
+end
