04d6a15b856ae75bbce853eca1b565fdcac51d57
Zachary P <zachary.s.prince@outlook.com>
2022-06-06 15:25:52 -0400

    Add popular ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,11 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+
+  def popular_ingredients
+    ingredients.group(:id)
+               .select('ingredients.*, count(ingredients) as ingredient_count')
+               .order(ingredient_count: :desc)
+               .limit(3)
+  end
 end
