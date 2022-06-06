25800b2364e7d7f14be9cac79d2b0dcbef0371a6
Luke Swenson <LukeSwenson95@outlook.com>
2022-06-06 15:29:46 -0600

    Finished User Story 1

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'Dishes Show Page', type: :feature do
   let!(:chef2) {Chef.create(name: "Gordon Ramsay")}
 
   let!(:dish1) {chef1.dishes.create(name: "Macaroni and Cheese", description: "Delicious pasta and cheese")}
-  let!(:dish2) {chef1.dishes.create(name: "Beef Wellington", description: "Medium Rare Beef with a bread cover")}
-  let!(:ingredient1) {dish1.ingredients.create(name: "Pasta", calories: "400")}
-  let!(:ingredient2) {dish1.ingredients.create(name: "Cheese", calories: "300")}
-  let!(:ingredient3) {dish2.ingredients.create(name: "Beef", calories: "250")}
-  let!(:ingredient4) {dish2.ingredients.create(name: "Bread", calories: "600")}
+  let!(:dish2) {chef2.dishes.create(name: "Beef Wellington", description: "Medium Rare Beef with a bread cover")}
+  let!(:ingredient1) {dish1.ingredients.create(name: "Pasta", calories: 400)}
+  let!(:ingredient2) {dish1.ingredients.create(name: "Cheese", calories: 300)}
+  let!(:ingredient3) {dish2.ingredients.create(name: "Beef", calories: 250)}
+  let!(:ingredient4) {dish2.ingredients.create(name: "Bread", calories: 600)}
 
   it "can display the dishes name and ingredients" do
     visit "/dishes/#{dish1.id}"
@@ -19,10 +19,17 @@ RSpec.describe 'Dishes Show Page', type: :feature do
       expect(page).to have_content("Delicious pasta and cheese")
       expect(page).to_not have_content("Beef Wellington")
       expect(page).to_not have_content("Medium Rare Beef with a bread cover")
+      expect(page).to_not have_content("Beef")
+    end
+
+    within '#ingredients' do
       expect(page).to have_content("Pasta")
       expect(page).to have_content("Cheese")
       expect(page).to_not have_content("Beef")
-      expect(page).to_not have_content("250")
+      expect(page).to_not have_content("Bread")
+    end
+
+    within '#chef' do
       expect(page).to have_content("Chef Boy R Dee")
       expect(page).to_not have_content("Gordon Ramsay")
     end
