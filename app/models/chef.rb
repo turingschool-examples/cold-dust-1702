6fe34c34317f6d0728a52820d0584ab0dcd71c62
Sage Skaff <skaffsage@gmail.com>
2022-06-06 14:57:50 -0700

    Update methods

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,6 @@ class Chef < ApplicationRecord
   has_many :dishes
   has_many :ingredients, through: :dishes
   def three_most_popular_ingredients
-    Chef.joins(:ingredients).group(:id).order('COUNT (ingredients.id) desc').distinct.limit(3)
+    # Chef.joins(:ingredients).group(:id).order('COUNT (ingredients.id) desc').distinct.limit(3)
   end
 end
