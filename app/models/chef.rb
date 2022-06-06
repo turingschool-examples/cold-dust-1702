f5ff5e608a954082f168dfd2f134ad2073c2ad9b
Sage Skaff <skaffsage@gmail.com>
2022-06-06 14:56:22 -0700

    Update three most popular ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,7 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+  def three_most_popular_ingredients
+    Chef.joins(:ingredients).group(:id).order('COUNT (ingredients.id) desc').distinct.limit(3)
+  end
 end
