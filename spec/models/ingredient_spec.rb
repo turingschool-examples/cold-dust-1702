3a7281bd56db07cca460076c1dc534eb41024b6a
Zachary P <zachary.s.prince@outlook.com>
2022-06-06 14:51:18 -0400

    Add ingredients relationship tests

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+  end
+  describe 'relationships' do
+    it { should have_many :dish_ingredients }
+    it { should have_many :dishes, through: :dish_ingredients }
+  end
+end
