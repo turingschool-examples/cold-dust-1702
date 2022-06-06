324efec343b5558aa646740f5319c9e183214f9b
Sarah Hall <swatson1994@yahoo.com>
2022-06-06 19:59:21 -0400

    Add to_not sections to test to make more robust

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe 'Merchant Show Dash', type: :feature do
     @noodle = Ingredient.create!(name: "Pasta", calories: 500)
     @sauce = Ingredient.create!(name: "Tomato Sauce", calories: 85)
     @cheese = Ingredient.create!(name: "Mozzarella", calories: 265)
-
     dish_ingredient1 = @spaghetti.dish_ingredients.create!(ingredient_id: @noodle.id)
     dish_ingredient2 = @spaghetti.dish_ingredients.create!(ingredient_id: @sauce.id)
     dish_ingredient3 = @spaghetti.dish_ingredients.create!(ingredient_id: @cheese.id)
+
+    @salad = @jimbob.dishes.create!(name: "Dinner Salad", description: "A lighter dinner option, with a lovely floral dressing")
+    @lettuce = Ingredient.create!(name: "Romaine Lettuce" , calories: 70)
+    @vinaigrette = Ingredient.create!(name: "Light Floral Vinaigrette" , calories: 90)
+    @croutons = Ingredient.create!(name: "Garlic Butter Croutons" , calories: 150)
+    dish_ingredient4 = @salad.dish_ingredients.create!(ingredient_id: @lettuce.id)
+    dish_ingredient5 = @salad.dish_ingredients.create!(ingredient_id: @vinaigrette.id)
+    dish_ingredient6 = @salad.dish_ingredients.create!(ingredient_id: @croutons.id)
   end
 
   it 'has a dishes name and description on the page' do
@@ -18,6 +25,8 @@ RSpec.describe 'Merchant Show Dash', type: :feature do
 
     expect(page).to have_content(@spaghetti.name)
     expect(page).to have_content(@spaghetti.description)
+    expect(page).to_not have_content(@salad.name)
+    expect(page).to_not have_content(@salad.description)
   end
 
   it 'has all ingredients listed' do
@@ -26,6 +35,7 @@ RSpec.describe 'Merchant Show Dash', type: :feature do
     expect(page).to have_content(@noodle.name)
     expect(page).to have_content(@sauce.name)
     expect(page).to have_content(@cheese.name)
+    expect(page).to_not have_content(@lettuce.name)
   end
 
   it "has the chef's name on the show page" do
@@ -39,5 +49,7 @@ RSpec.describe 'Merchant Show Dash', type: :feature do
 
     expect(page).to have_content(850)
     expect(page).to have_content("Total calories: 850")
+    expect(page).to_not have_content(310)
+    expect(page).to_not have_content("Total calories: 310")
   end
 end
