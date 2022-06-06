cbf27c6ef3997a85eb356e65c200db0620dbb390
Luke Swenson <LukeSwenson95@outlook.com>
2022-06-06 15:36:58 -0600

    Created model method for total_calories

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,9 @@ class Dish < ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
   has_many :ingredients
+
+
+def total_calories
+  ingredients.sum(:calories)
+  end
 end
