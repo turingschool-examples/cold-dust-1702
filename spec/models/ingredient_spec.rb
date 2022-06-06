f26614370a88ee2fb41d3111233df94b2a0ae759
Zachary P <zachary.s.prince@outlook.com>
2022-06-06 14:54:41 -0400

    Fix validation and relationship tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Dish, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :description }
-  end
+RSpec.describe Ingredient, type: :model do
   describe 'relationships' do
     it { should have_many :dish_ingredients }
-    it { should have_many :dishes, through: :dish_ingredients }
+    it { should have_many :dishes }
   end
 end
