fd01087a856aa79b0aacc2c9ff4bb84e07a87773
tjhaines-cap <tjhaines@eckerd.edu>
2022-06-06 16:56:12 -0600

    Feat: add chef name and link to ingredients list to chef show page

---
+++ app/controllers/chef_ingredients_controller.rb
@@ -0,0 +1,7 @@
+class ChefIngredientsController < ApplicationController
+
+  def index
+
+  end
+  
+end
--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,7 @@
 class ChefsController < ApplicationController
 
   def show
-
+    @chef = Chef.find(params[:id])
   end
-  
+
 end
