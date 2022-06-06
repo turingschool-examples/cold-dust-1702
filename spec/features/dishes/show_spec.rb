73f63b52cdc27157dbdf15ae4369084eeea9e5d4
Zachary P <zachary.s.prince@outlook.com>
2022-06-06 13:52:58 -0400

    Add dish show page test

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'dish show page' do
+  it 'display the dish name, description, ingredients, and chef name' do
+    chef = Chef.create!(name: 'Alfonso')
+    dish = chef.dishes.create!(name: 'Sorry Salad', description: 'I am so sorry i made this.')
+    lettuce = Ingredient.create!(name: 'lettuce', calories: 0)
+    tears = Ingredient.create!(name: 'tears', calories: 12)
+    gluten_free_croutons = Ingredient.create!(name: 'Gluten-Free Croutons', calories: 129)
+    DishIngredient.create!(dish_id: dish.id, ingredient_id: lettuce.id)
+    DishIngredient.create!(dish_id: dish.id, ingredient_id: tears.id)
+    DishIngredient.create!(dish_id: dish.id, ingredient_id: gluten_free_croutons.id)
+
+    visit "/dishes/#{dish.id}"
+
+    expect(page).to have_content('Sorry Salad')
+
+    expect(page).to have_content('I am so sorry I made this.')
+
+    expect(page).to have_content('lettuce')
+
+    expect(page).to have_content('tears')
+
+    expect(page).to have_content('Gluten-Free Croutons')
+
+    expect(page).to have_content('Alfonso')
+  end
+end
