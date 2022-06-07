99a3de0607292ee703972160229c2857ef7ab8ba
Cory Bethune <corybbethune@gmail.com>
2022-06-06 23:24:26 -0700

    Add the_cook method to dish. Update the view. Test passes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,11 @@
 class Dish < ApplicationRecord
   validates_presence_of :name, :description
-  
+
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def the_cook
+    self.chef.name
+  end
 end
