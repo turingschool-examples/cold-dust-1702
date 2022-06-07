1ece2b2970b8b6ddcc6a9efb494825cf600b53f4
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-07 04:48:05 -0600

    create table relationships within models; clarify confusing pluralization conventions

--- spec/models/dishes_ingredients_spec.rb
+++ spec/models/dishes_ingredients_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe DishIngredient, type: :model do
+RSpec.describe DishesIngredient, type: :model do
   describe 'relationships' do
     it { should belong_to(:dish) }
     it { should belong_to(:ingredient) }
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
     describe 'relationships' do
-    it {should have_many(:dish_ingredients) }
+    it {should have_many(:dishes_ingredients) }
     it {should have_many(:dishes).through(:dishes_ingredients)}
   end
 end
