a2ad0eda44d9f3ac69c028686ee236c4a19ead52
Matt Deming <vballmatt44@gmail.com>
2022-06-07 15:31:30 -0600

    Add all relationships and passing tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ingredient do
   describe 'relationships' do
-    it { should have_many :dishes }
+    it { should have_many :dish_ingredients }
+    it { should have_many(:dishes).through(:dish_ingredients) }
   end
 end
