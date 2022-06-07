1a521cc8729410a614940bc23c5dd43ce8a8b61f
Sarah Hall <swatson1994@yahoo.com>
2022-06-06 20:03:07 -0400

    Add initial test for chefs show page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Dish Show Page', type: :feature do
+RSpec.describe 'Chef Show Page', type: :feature do
   before(:each) do
     @jimbob = Chef.create!(name: "Jimbob Dudeguy")
     @spaghetti = @jimbob.dishes.create!(name: "Spaghetti" , description: "Wholesome ol' carbs, tomato, and cheese")
@@ -20,36 +20,9 @@ RSpec.describe 'Dish Show Page', type: :feature do
     dish_ingredient6 = @salad.dish_ingredients.create!(ingredient_id: @croutons.id)
   end
 
-  it 'has a dishes name and description on the page' do
-    visit "/dishes/#{@spaghetti.id}"
-
-    expect(page).to have_content(@spaghetti.name)
-    expect(page).to have_content(@spaghetti.description)
-    expect(page).to_not have_content(@salad.name)
-    expect(page).to_not have_content(@salad.description)
-  end
-
-  it 'has all ingredients listed' do
-    visit "/dishes/#{@spaghetti.id}"
-
-    expect(page).to have_content(@noodle.name)
-    expect(page).to have_content(@sauce.name)
-    expect(page).to have_content(@cheese.name)
-    expect(page).to_not have_content(@lettuce.name)
-  end
-
-  it "has the chef's name on the show page" do
-    visit "/dishes/#{@spaghetti.id}"
+  it 'has the name of the chef' do
+    visit "/chefs/#{@jimbob.id}"
 
     expect(page).to have_content(@jimbob.name)
-  end
-
-  it 'has total calorie count listed' do
-    visit "/dishes/#{@spaghetti.id}"
-
-    expect(page).to have_content(850)
-    expect(page).to have_content("Total calories: 850")
-    expect(page).to_not have_content(310)
-    expect(page).to_not have_content("Total calories: 310")
   end
 end
