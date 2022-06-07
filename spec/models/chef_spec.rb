0b8b62f7323c909f67f3ec9c232ccc130d965db1
Andrew King <dev.andrewking@gmail.com>
2022-06-06 22:47:16 -0500

    add model test for popular ingredients

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -31,4 +31,32 @@ RSpec.describe Chef, type: :model do
       expect(@chef.chef_ingredients.to_a).to eq([@ingredient, @ingredient2, @ingredient3, @ingredient4])
     end
   end
+
+  describe "#popular_ingredients" do
+    before :each do
+        @chef = Chef.create!(name: "Chef Boyardee")
+        @chef2 = Chef.create!(name: "Chef Ramsay")
+        @dish = @chef.dishes.create!(name: "Ravioli", description: "Cheap.")
+        @dish2 = @chef.dishes.create!(name: "Lasagna", description: "Also cheap.")
+        @dish3 = @chef.dishes.create!(name: "Spaghetti", description: "Cheaper.")
+        @ingredient = Ingredient.create!(name: "Sodium", calories: 400)
+        @ingredient2 = Ingredient.create!(name: "Sauce", calories: 350)
+        @ingredient3 = Ingredient.create!(name: "Sugar", calories: 550)
+        @ingredient4 = Ingredient.create!(name: "Noodles", calories: 600)
+        @ingredient5 = Ingredient.create!(name: "Parmesan", calories: 100)
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient2.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient3.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient2.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient5.id}")
+    end
+
+    it 'shows 3 most popular ingredients used by chef' do
+      expect(@chef.popular_ingredients).to eq([@ingredient4, @ingredient2, @ingredient])
+    end
+  end
 end
