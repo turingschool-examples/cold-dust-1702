da7187f21fc372ec260488ab0c26eaa6f5fd525f
Cory Bethune <corybbethune@gmail.com>
2022-06-06 23:59:58 -0700

    Update chef model and test with more associations

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'chef show page' do
+  before(:each) do
+    @chef1 = Chef.create!(name: 'Cory')
+    @chef2 = Chef.create!(name: 'Kyle')
+
+    @dish1 = @chef1.dishes.create!(name: "Pizza", description: 'I love it.')
+    @dish2 = @chef1.dishes.create!(name: "Pasta", description: 'I kinda love it.')
+    @dish3 = @chef2.dishes.create!(name: "Collard Greens", description: 'I hate it.')
+    @dish4 = @chef2.dishes.create!(name: "Chives", description: 'I kinda hate it.')
+
+    @ingredient1 = Ingredient.create!(name: 'Pepperoni', calories: 200)
+    @ingredient2 = Ingredient.create!(name: 'Cheese', calories: 20)
+    @ingredient3 = Ingredient.create!(name: 'Leafy Greens', calories: 10)
+    @ingredient4 = Ingredient.create!(name: 'Green Onion', calories: 100)
+    @ingredient5 = Ingredient.create!(name: 'Extra Pepperoni', calories: 400)
+
+
+    @di1 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient1.id)
+    @di2 = DishIngredient.create!(dish_id: @dish2.id, ingredient_id: @ingredient2.id)
+    @di3 = DishIngredient.create!(dish_id: @dish3.id, ingredient_id: @ingredient3.id)
+    @di4 = DishIngredient.create!(dish_id: @dish4.id, ingredient_id: @ingredient4.id)
+    @di5 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient5.id)
+  end
+
+  it "shows the chefs name and lists all the ingredients of their dishes" do
+    visit "/chefs/#{@chef1.id}"
+  end
+end
