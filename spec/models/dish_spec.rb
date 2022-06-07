e307448cf4e643f15c3dc055b5d2fffa6cfb0302
Matt Deming <vballmatt44@gmail.com>
2022-06-07 16:08:47 -0600

    Add test for calorie count on dish model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe "class methods" do
+    describe "#dish_calories" do
+      it "can count the calories in a dish" do
+        lou = Chef.create!(name: "Lou Baron")
+        pizza = lou.dishes.create!(name: "Cheese Pizza", description: "A delightfully light cheese covered peice of bread!")
+        calzone = lou.dishes.create!(name: "Beef Calzone", description: "Beef and cheese in a folded peice of bread!")
+        cheese = Ingredient.create!(name: "Cheese", calories: 25)
+        beef = Ingredient.create!(name: "Beef", calories: 50)
+        bread = Ingredient.create!(name: "Bread", calories: 500)
+        dish_ingredient1 = DishIngredient.create!(dish_id: pizza.id, ingredient_id: cheese.id)
+        dish_ingredient1 = DishIngredient.create!(dish_id: pizza.id, ingredient_id: bread.id)
+        dish_ingredient1 = DishIngredient.create!(dish_id: calzone.id, ingredient_id: cheese.id)
+        dish_ingredient1 = DishIngredient.create!(dish_id: calzone.id, ingredient_id: beef.id)
+        dish_ingredient1 = DishIngredient.create!(dish_id: calzone.id, ingredient_id: bread.id)
+
+        expect(Dish.dish_calories).to eq(575)
+      end
+    end
+  end
 end
