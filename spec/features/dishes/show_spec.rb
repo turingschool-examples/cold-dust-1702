cc7614c344dcaa0c9d755e0824f8ab5b21860c26
Andrew King <dev.andrewking@gmail.com>
2022-06-06 20:31:48 -0500

    init commit

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'dish show page' type: :feature do
+    it 'shows dish name and description' do
+
+    end
+
+    it 'shows a list of ingredients' do
+
+    end
+
+    it 'shows the chefs name' do
+
+    end
+end
