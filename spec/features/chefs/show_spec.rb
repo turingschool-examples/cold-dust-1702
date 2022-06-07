c2da8c45130bf008930b0fb99b60a8027a311cd2
Andrew King <dev.andrewking@gmail.com>
2022-06-06 22:44:31 -0500

    add popular ingredients feature test

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -2,19 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'chef show page', type: :feature do
     before :each do
-        @chef = Chef.create(name: "Chef Boyardee")
-        @chef2 = Chef.create(name: "Chef Ramsay")
-        @dish = @chef.dishes.create(name: "Ravioli", description: "Cheap.")
-        @dish2 = @chef.dishes.create(name: "Lasagna", description: "Also cheap.")
-        @ingredient = Ingredient.create(name: "Sodium", calories: 400)
-        @ingredient2 = Ingredient.create(name: "Sauce", calories: 350)
-        @ingredient3 = Ingredient.create(name: "Sugar", calories: 550)
-        @ingredient4 = Ingredient.create(name: "Noodles", calories: 600)
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient.id}")
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient2.id}")
-        DishIngredient.create(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient3.id}")
-        DishIngredient.create(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient4.id}")
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient4.id}")
+        @chef = Chef.create!(name: "Chef Boyardee")
+        @chef2 = Chef.create!(name: "Chef Ramsay")
+        @dish = @chef.dishes.create!(name: "Ravioli", description: "Cheap.")
+        @dish2 = @chef.dishes.create!(name: "Lasagna", description: "Also cheap.")
+        @dish3 = @chef.dishes.create!(name: "Spaghetti", description: "Cheaper.")
+        @ingredient = Ingredient.create!(name: "Sodium", calories: 400)
+        @ingredient2 = Ingredient.create!(name: "Sauce", calories: 350)
+        @ingredient3 = Ingredient.create!(name: "Sugar", calories: 550)
+        @ingredient4 = Ingredient.create!(name: "Noodles", calories: 600)
+        @ingredient5 = Ingredient.create!(name: "Parmesan", calories: 100)
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient2.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient3.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient2.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient.id}")
+        DishIngredient.create!(dish_id: "#{@dish3.id}", ingredient_id: "#{@ingredient5.id}")
     end
 
 
@@ -28,6 +34,17 @@ RSpec.describe 'chef show page', type: :feature do
         visit "/chefs/#{@chef.id}"
         click_link "Ingredient Index"
         expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
+    end
+
+    it 'has a list of 3 most popular ingredients' do
+        visit "/chefs/#{@chef.id}"
+        within "#popular_ingredients" do
+            expect(page).to have_content("Noodles")
+            expect(page).to have_content("Sauce")
+            expect(page).to have_content("Sodium")
+            expect(page).to_not have_content("Sugar")
+            expect(page).to_not have_content("Parmesan")
+        end
     end
 
 end
