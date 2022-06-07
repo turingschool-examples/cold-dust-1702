ac672cbacc5b988c93c12cab1dd908ebc26efd96
Sarah Hall <swatson1994@yahoo.com>
2022-06-06 20:12:39 -0400

    Add tests for chefs ingredient index link

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Chef Show Page', type: :feature do
   before(:each) do
     @jimbob = Chef.create!(name: "Jimbob Dudeguy")
+    @pepsi = Chef.create!(name: "Ivanna Pepsi")
     @spaghetti = @jimbob.dishes.create!(name: "Spaghetti" , description: "Wholesome ol' carbs, tomato, and cheese")
     @noodle = Ingredient.create!(name: "Pasta", calories: 500)
     @sauce = Ingredient.create!(name: "Tomato Sauce", calories: 85)
@@ -24,5 +25,20 @@ RSpec.describe 'Chef Show Page', type: :feature do
     visit "/chefs/#{@jimbob.id}"
 
     expect(page).to have_content(@jimbob.name)
+    expect(page).to_not have_content(@pepsi.name)
+  end
+
+  it 'has a link to a chefs ingredient index page' do
+    visit "/chefs/#{@jimbob.id}"
+
+    expect(page).to have_link("Jimbob Dudeguy's Ingredients")
+  end
+
+  it 'has a working link to the chefs ingredients page' do
+    visit "/chefs/#{@jimbob.id}"
+
+    click_link("Jimbob Dudeguy's Ingredients")
+
+    expect(page).to have_current_path("/chefs/#{@jimbob.id}/ingredients")
   end
 end
