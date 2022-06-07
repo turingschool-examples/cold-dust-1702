98e18e819835adbfd9a41499c0947f98c4ff0d4e
Andrew King <dev.andrewking@gmail.com>
2022-06-06 22:32:51 -0500

    add bang to creates

--- spec/features/chef_ingredients/index_spec.rb
+++ spec/features/chef_ingredients/index_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'chef ingredient index page', type: :feature do
     before :each do
-        @chef = Chef.create(name: "Chef Boyardee")
-        @chef2 = Chef.create(name: "Chef Ramsay")
-        @dish = @chef.dishes.create(name: "Ravioli", description: "Cheap.")
-        @dish2 = @chef.dishes.create(name: "Lasagna", description: "Also cheap.")
-        @ingredient = Ingredient.create(name: "Sodium", calories: 400)
-        @ingredient2 = Ingredient.create(name: "Sauce", calories: 350)
-        @ingredient3 = Ingredient.create(name: "Sugar", calories: 550)
-        @ingredient4 = Ingredient.create(name: "Noodles", calories: 600)
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient.id}")
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient2.id}")
-        DishIngredient.create(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient3.id}")
-        DishIngredient.create(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient4.id}")
-        DishIngredient.create(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient4.id}")
+        @chef = Chef.create!(name: "Chef Boyardee")
+        @chef2 = Chef.create!(name: "Chef Ramsay")
+        @dish = @chef.dishes.create!(name: "Ravioli", description: "Cheap.")
+        @dish2 = @chef.dishes.create!(name: "Lasagna", description: "Also cheap.")
+        @ingredient = Ingredient.create!(name: "Sodium", calories: 400)
+        @ingredient2 = Ingredient.create!(name: "Sauce", calories: 350)
+        @ingredient3 = Ingredient.create!(name: "Sugar", calories: 550)
+        @ingredient4 = Ingredient.create!(name: "Noodles", calories: 600)
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient2.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient3.id}")
+        DishIngredient.create!(dish_id: "#{@dish2.id}", ingredient_id: "#{@ingredient4.id}")
+        DishIngredient.create!(dish_id: "#{@dish.id}", ingredient_id: "#{@ingredient4.id}")
     end
 
 
