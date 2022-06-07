9c615cb0097a8e30ef89f27b7484304e882bd9cc
Sarah Hall <swatson1994@yahoo.com>
2022-06-06 20:54:52 -0400

    Add method for showing all unique ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,8 @@
 class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+
+  def all_ingredients
+    dishes.joins(ingredients: :dish_ingredients).select('ingredients.name').distinct
+  end
 end
