87b3099fe2deeaed4d5ce57fe32f54917b3087dd
Deannah Burke <deannah@bierstadtlager.com>
2022-06-06 18:18:07 -0600

    complete user story 1

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe 'the dish show page' do
   before(:each)do
     @wolfgang = Chef.create!(name: "Wolfgang Puck")
 
-    @spaghetti = wolfgang.dishes.create!(name: "Spaghetti", description: "Red sauce and meatballs")
-    @penne = wolfgang.dishes.create!(name: "Penne", description: "White Sauce and spinach")
-    @macaroni = wolfgang.dishes.create!(name: "Macaroni", description: "Four cheese")
+    @spaghetti = @wolfgang.dishes.create!(name: "Spaghetti", description: "Red sauce and meatballs")
+    @penne = @wolfgang.dishes.create!(name: "Penne", description: "White Sauce and spinach")
+    @macaroni = @wolfgang.dishes.create!(name: "Macaroni", description: "Four cheese")
 
     @pasta = Ingredient.create!(name: "Pasta noodles", calories: 300)
     @tomatoes = Ingredient.create!(name: "Tomatoes", calories: 25)
@@ -20,11 +20,21 @@ RSpec.describe 'the dish show page' do
     @cheese = Ingredient.create!(name: "Parmesan", calories: 215)
     @veggies = Ingredient.create!(name: "Vegetable blend", calories: 65)
 
-    DishIngredient.create!(dish: spaghetti, ingredient: pasta)
-    DishIngredient.create!(dish: spaghetti, ingredient: tomatoes)
-    DishIngredient.create!(dish: penne, ingredient: sauce)
-    DishIngredient.create!(dish: penne, ingredient: veggies)
-    DishIngredient.create!(dish: macaroni, ingredient: pasta)
-    DishIngredient.create!(dish: macaroni, ingredient: cheese)
+    DishIngredient.create!(dish: @spaghetti, ingredient: @pasta)
+    DishIngredient.create!(dish: @spaghetti, ingredient: @tomatoes)
+    DishIngredient.create!(dish: @penne, ingredient: @sauce)
+    DishIngredient.create!(dish: @penne, ingredient: @veggies)
+    DishIngredient.create!(dish: @macaroni, ingredient: @pasta)
+    DishIngredient.create!(dish: @macaroni, ingredient: @cheese)
+  end
+
+  it 'displays dish name, ingredients, and chef' do
+    visit "/dishes/#{@spaghetti.id}"
+
+    expect(page).to have_content("Spaghetti")
+    expect(page).to have_content("Red sauce and meatballs")
+    expect(page).to have_content("Pasta noodles")
+    expect(page).to have_content("Tomatoes")
+    expect(page).to have_content("Wolfgang Puck")
   end
 end
