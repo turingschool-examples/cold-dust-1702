069edf98c25099f60faeda04dcb1f23497132017
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-07 15:43:24 -0700

    story 1 of 3

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe 'the dish show page' do
     ingredient1 = Ingredient.create!(name: "cheese", calories: 120)
     ingredient2 = Ingredient.create!(name: "chips", calories: 140)
     ingredient3 = Ingredient.create!(name: "jalapenos", calories: 30)
+    dishingredient1 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient1.id)
+    dishingredient2 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient2.id)
+    dishingredient3 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient3.id)
     visit "/dishes/#{dish1.id}"
     expect(page).to have_content("Name: #{dish1.name}")
     expect(page).to have_content("description: #{dish1.description}")
-    expect(page).to have_content("ingredients: #{ingredient1.name}, #{ingredient2.name}, #{ingredient3.name}")
+    expect(page).to have_content(ingredient1.name)
+    expect(page).to have_content(ingredient2.name)
+    expect(page).to have_content(ingredient3.name)
     expect(page).to have_content("chef: #{chef1.name}")
   end
 end
