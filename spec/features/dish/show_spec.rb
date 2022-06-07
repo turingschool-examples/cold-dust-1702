f2653c1a4fe3e9b2717c1a63524d2b71304f49a1
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-07 15:55:57 -0700

    fix test

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -20,6 +20,15 @@ RSpec.describe 'the dish show page' do
   end
 
   it 'shows total calorie count' do
+    chef1 = Chef.create!(name: "Eric")
+    dish1 = chef1.dishes.create!(name: "nachos", description: "cheesy dish!")
+    ingredient1 = Ingredient.create!(name: "cheese", calories: 120)
+    ingredient2 = Ingredient.create!(name: "chips", calories: 140)
+    ingredient3 = Ingredient.create!(name: "jalapenos", calories: 30)
+    dishingredient1 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient1.id)
+    dishingredient2 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient2.id)
+    dishingredient3 = DishIngredient.create!(dish_id: dish1.id, ingredient_id: ingredient3.id)
+
     visit "/dishes/#{dish1.id}"
     expect(page).to have_content("total calories: 290")
   end
